391eca08eeaa5320ef5139bc18e294b422a138ec
Arijit Pan <arijitpan712407@gmail.com>
2026-09-21 23:43:24 +0530

    Time: 351 ms (76.32%) | Memory: 34.2 MB (69.74%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -0,0 +1,24 @@
+class Solution:
+    def resultArray(self, nums: List[int], k: int) -> List[int]:
+        ans = [0] * k
+        prev = [0] * k
+
+        for num in nums:
+            curr = [0] * k
+
+            # Subarray containing only num
+            curr[num % k] += 1
+
+            # Extend all previous subarrays
+            for r in range(k):
+                if prev[r] > 0:
+                    new_r = (r * num) % k
+                    curr[new_r] += prev[r]
+
+            # Add subarrays ending at this position
+            for r in range(k):
+                ans[r] += curr[r]
+
+            prev = curr
+
+        return ans
